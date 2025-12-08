35d09717fccf40f1c4d0896b5824782437891489
Ryu_12 <shiryu.1211@icloud.com>
2025-12-09 00:21:15 +0900

    Delete public directory

--- public/main.js
+++
@@ -1,8 +0,0 @@
-// ここからコードを書いてください
-import { setupTabs } from "./js/tabs.js";
-import { setupConverter } from "./js/converter.js";
-
-document.addEventListener("DOMContentLoaded", () => {
-  setupTabs();
-  setupConverter();
-});
