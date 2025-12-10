c9cb2c748650f8400365ace85570088861bd410e
Ryu_12 <shiryu.1211@icloud.com>
2025-12-11 00:20:53 +0900

    Delete public directory

--- public/main.js
+++
@@ -1,9 +0,0 @@
-import { setupTabs } from "./js/tabs.js";
-import { setupConverter } from "./js/converter.js";
-import { setupFlashcards } from "./js/flashcards.js";
-
-document.addEventListener("DOMContentLoaded", () => {
-  setupTabs();
-  setupConverter();
-  setupFlashcards();
-});
